005f208213f54a8478be1d94bba7fe1514b370a4
fayad <fayad@brynmawr.edu>
2024-11-26 23:09:37 -0500

    hi

--- MainGame.py
+++ MainGame.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 print("Hello there!")
 print("Welcome to Hilles Hall where you have Python for lunch and Java for Dinner")
 print("You have been programming all night upstairs and you've been locked up")
-print("You are looking for keys to get out of the library")
+print("You are looking for keys to get out of the Lab H204")
 
 
 while True:
